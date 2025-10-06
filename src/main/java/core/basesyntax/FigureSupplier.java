407dbb544c620649d4d4cd5bfc6c23cde297e5c3
Vitaliy Tishik <vitisik54@gmail.com>
2025-10-06 17:19:08 +0300

    refactor

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,26 +4,44 @@ import java.util.Random;
 
 public class FigureSupplier {
     private static final int MAX_SIZE = 20;
+    private static final int FIGURE_TYPE_COUNT = 5;
+    private static final int DEFAULT_RADIUS = 10;
     private final Random random = new Random();
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
-        int figureType = random.nextInt(5);
+        int figureType = random.nextInt(FIGURE_TYPE_COUNT);
         String color = colorSupplier.getRandomColor();
 
         switch (figureType) {
-            case 0: return new Square(color, random.nextInt(MAX_SIZE) + 1);
-            case 1: return new Rectangle(color, random.nextInt(MAX_SIZE) + 1,
-                    random.nextInt(MAX_SIZE) + 1);
-            case 2: return new RightTriangle(color, random.nextInt(MAX_SIZE) + 1,
-                    random.nextInt(MAX_SIZE) + 1);
-            case 3: return new Circle(color, random.nextInt(MAX_SIZE) + 1);
-            default: return new IsoscelesTrapezoid(color, random.nextInt(MAX_SIZE) + 1,
-                    random.nextInt(MAX_SIZE) + 1, random.nextInt(MAX_SIZE) + 1);
+            case 0: {
+                double side = random.nextInt(MAX_SIZE) + 1;
+                return new Square(color, side);
+            }
+            case 1: {
+                double width = random.nextInt(MAX_SIZE) + 1;
+                double height = random.nextInt(MAX_SIZE) + 1;
+                return new Rectangle(color, width, height);
+            }
+            case 2: {
+                double firstLeg = random.nextInt(MAX_SIZE) + 1;
+                double secondLeg = random.nextInt(MAX_SIZE) + 1;
+                return new RightTriangle(color, firstLeg, secondLeg);
+            }
+            case 3: {
+                double radius = random.nextInt(MAX_SIZE) + 1;
+                return new Circle(color, radius);
+            }
+            default: {
+                double base1 = random.nextInt(MAX_SIZE) + 1;
+                double base2 = random.nextInt(MAX_SIZE) + 1;
+                double height = random.nextInt(MAX_SIZE) + 1;
+                return new IsoscelesTrapezoid(color, base1, base2, height);
+            }
         }
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(Color.WHITE.name(), 10);
+        return new Circle(Color.WHITE.name(), DEFAULT_RADIUS);
     }
 }
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,9 +1,11 @@
 package core.basesyntax;
 
 public class Main {
+    private final static int FIGURE_COUNT = 6;
+
     public static void main(String[] args) {
         FigureSupplier supplier = new FigureSupplier();
-        Figure[] figures = new Figure[6];
+        Figure[] figures = new Figure[FIGURE_COUNT];
 
         for (int i = 0; i < figures.length / 2; i++) {
             figures[i] = supplier.getRandomFigure();
